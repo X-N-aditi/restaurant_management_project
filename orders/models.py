72ef08b5ea53070e3754a9d2b7d4830798e0adc7
X-N-aditi <aditishukla564@gmail.com>
2025-08-02 08:16:47 +0530

    16972

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Order Model Table
+class Order(models.Model):
+    order_id = models.CharField(max_length=50, unique=True)
+    note = models.TextField()
+    customer_name = models.CharField(max_length=100)
+    total_order_amount = models.DecimalField(decimal_places=2, max_digits=10)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.order_id} order status: {self.is_paid}"
+
+    
+# OrderItem Model Table
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=100)
+    item_price = models.DecimalField(decimal_places=2, max_digits=10)
+    quantity = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item_price and self.quantity
