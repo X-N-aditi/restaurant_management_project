ea4727f39f13fee085e15d0c3af70181f74ba2eb
X-N-aditi <aditishukla564@gmail.com>
2025-08-04 08:55:10 +0530

    16976

--- Blog/BlogPost/models.py
+++ Blog/BlogPost/models.py
@@ -20,3 +20,24 @@ from rest_framework import viewsets
 class BlogPostView(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all().order_by('-created_at')
     serializer_class = BlogPostSerializer
+
+
+# views.py
+
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class BlogSearchAPIView(APIView):
+    def get(self, request):
+        query = request.GET.get("search", '')
+        if query:
+            posts = BlogPost.objects.filter(Q(title_icontains=query) | Q(content_icontains=query))
+        else:
+            posts = BlogPost.objects.none()
+            serializers = BlogPostSerializer(posts, many=True)
+            return Response(serializers.data, status=status.HTTP_200_OK)
