47fad2e57717584e3291a48d5130d79acc40a724
X-N-aditi <aditishukla564@gmail.com>
2025-08-05 09:57:17 +0530

    21665

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User_profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'user')
+    name = models.CharaField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharaField(max_length15)
+
+    def __str__(self):
+        return self.name
